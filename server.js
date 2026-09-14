7727ec2ebbdcbe94214a6ecf20434e60faef1015
sonam <kumarisonam08685@gmail.com>
2026-09-14 11:47:49 +0530

    Organize backend project structure

---
+++ server.js
@@ -0,0 +1,11 @@
+
+require("dotenv").config()
+const app = require("./src/app")
+const connectToDB = require("./src/config/db")
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+connectToDB()
+
+app.listen(3000,() =>{
+    console.log("server is running on port 3000")
+})
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+const userModel = require("../models/user.model");
+
+/**
+ * @route POST/api/auth/register
+ * @description Register a new user,expects username,email and password in the request body
+ * @access Public
+ */
+async function registerUserController(req, res) {}
+
+module.exports = {
+    registerUserController
+}
