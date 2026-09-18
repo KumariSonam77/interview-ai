dc1b100a88ab0637f8dc586d2c6c5eb485b47b4b
sonam <kumarisonam08685@gmail.com>
2026-09-18 09:56:30 +0530

    feat: update authentication and logout flow

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authController = require("../controllers/auth.controller");
 const authRouter = express.Router();
+const authMiddleware = require("../middlewares/auth.middleware")
 
 /**
  * @route POST/api/auth/register
@@ -24,5 +25,13 @@ authRouter.post("/login",authController.loginUserController)
  */
 
 authRouter.get("/logout",authController.logoutUserController)
+
+/**
+ * @route GET/api/auth/get-me
+ * @description get the current logged in user details
+ * @access private
+ */
+authRouter.get("/get-me",authMiddleware.authUser,authController.getMeController)
+ 
 
 module.exports = authRouter
