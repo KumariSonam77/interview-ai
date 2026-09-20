dea2ae4804042579dd679085b439863a531ae535
sonam <kumarisonam08685@gmail.com>
2026-09-20 08:52:44 +0530

    feat: add frontend authentication services

--- Frontend/src/Features/auth/pages/login.jsx
+++ Frontend/src/Features/auth/pages/login.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import {useNavigate,Link} from "react-router"
+import"../auth.form.scss"
 
 const login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>login</div>
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" placeholder='Enter email address'/>
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">password</label>
+            <input type="password" id="password" name="password" placeholder='Enter password'/>
+          </div>
+          <button className='button primary-button'>login</button>
+        </form>
+
+        <p> Don't have an account? <Link to={"/register"}>Register</Link></p>
+      </div>
+    </main>
   )
 }
 
